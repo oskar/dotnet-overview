d43e11620d25f1c837b516e83b597adb6a0859f4
Oskar Hermansson <oskar.hermansson@gmail.com>
2018-11-02 18:14:08 +0100

    Add simple tests for ProjectParser and Project

---
+++ DotNetOverview.Library.Tests/ProjectTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace DotNetOverview.Library.Tests
+{
+  public class ProjectTests
+  {
+    [Fact]
+    public void Constructor_creates_object_without_name() =>
+      Assert.Null(new Project().Name);
+
+    [Fact]
+    public void Constructor_creates_object_without_path() =>
+      Assert.Null(new Project().Path);
+
+    [Fact]
+    public void Constructor_creates_object_without_default_value_for_NewCsProjFormat() =>
+      Assert.Null(new Project().NewCsProjFormat);
+
+    [Fact]
+    public void Constructor_creates_object_without_default_value_for_TargetFramework() =>
+      Assert.Null(new Project().TargetFramework);
+
+    [Fact]
+    public void Constructor_creates_object_without_default_value_for_TreatWarningsAsErrors() =>
+      Assert.Null(new Project().TreatWarningsAsErrors);
+  }
+}

3c87bd0de6b2bcc05e2d3a3f53d4e9969af2b2e9
Oskar Hermansson <oskar.hermansson@gmail.com>
2025-09-07 13:05:49 +0200

    Rename csproj files and move to src/

--- build/globaltool-reinstall-local.sh
+++ build/globaltool-reinstall-local.sh
@@ -1,3 +1,3 @@
-dotnet pack src -o src/out
+dotnet pack -o out
 dotnet tool uninstall -g dotnet-overview
-dotnet tool install -g dotnet-overview --add-source src/out --no-cache
+dotnet tool install -g dotnet-overview --add-source out --no-cache

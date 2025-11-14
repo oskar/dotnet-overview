b1c0d98be370a3c2212e743b57d9c9333fca3925
Oskar Hermansson <oskar.hermansson@gmail.com>
2025-11-14 13:30:09 +0100

    Make project Path and Name required and non-nullable

--- src/DotNetOverview/Project.cs
+++ src/DotNetOverview/Project.cs
@@ -2,10 +2,10 @@ namespace DotNetOverview;
 
 public class Project
 {
-    public string? Name { get; set; }
-    public string? Path { get; set; }
-    public bool? SdkFormat { get; set; }
+    public required string Name { get; set; }
+    public required string Path { get; set; }
 
+    public bool? SdkFormat { get; set; }
     public string? TargetFramework { get; set; }
     public string? OutputType { get; set; }
     public string? Authors { get; set; }

6d05d81353b7380bdc3497673b94894a3b4c2de8
Oskar Hermansson <oskar.hermansson@gmail.com>
2025-11-14 13:29:05 +0100

    Remove unused property

--- src/DotNetOverview/Project.cs
+++ src/DotNetOverview/Project.cs
@@ -7,7 +7,6 @@ public class Project
     public bool? SdkFormat { get; set; }
 
     public string? TargetFramework { get; set; }
-    public bool? TreatWarningsAsErrors { get; set; }
     public string? OutputType { get; set; }
     public string? Authors { get; set; }
     public string? Version { get; set; }
